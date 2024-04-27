5961121344b719477549a5bf5c66117c1dec770d
Vadim <vadim.galian@gmail.com>
2024-04-27 13:28:43 +0300

    27.04

--- hashObj.js
+++ hashObj.js
@@ -4,16 +4,23 @@ class Astrobject {
 
     constructor() {
         this.MAX_NUMB = 101
-        this.arr = new Array(this.MAX_NUMB).fill(undefined)
+        this.arr = new Array(this.MAX_NUMB).fill(undefined).map(() => [])
     }
 
     castStringToInt(str) {
-        return str.length % this.MAX_NUMB
+        const sum = str.split("").reduce((acc, value) => acc + value.charCodeAt(), 0)
+
+        return sum % this.MAX_NUMB
     }
 
     set(key, value) {
         const newKey = this.castStringToInt(key)
-        this.arr[newKey] = [key, value]
+        const index = this.arr[newKey].findIndex(([itemKey]) => itemKey === key)
+        if (index === -1) {
+            this.arr[newKey].push([key, value])
+        } else {
+            this.arr[newKey][index][1] = value
+        }
     }
 
     get(key) {
@@ -35,9 +42,20 @@ class Astrobject {
 
 const astraObj = new Astrobject()
 
-astraObj.set("hello", "world")
-astraObj.set("1", "1")
+astraObj.set("abc", "world")
+astraObj.set("abc", "hello")
+astraObj.set("acb", "hello1")
+astraObj.set("acb", "hello2")
 
-console.log(astraObj.get("hello"))
+// astraObj.set("1", "1")
+// console.log(astraObj.get("hello"))
 
-console.log(astraObj.getAll())
+console.log(astraObj.arr)
+
+// console.log(astraObj.castStringToInt("acb"))
+// console.log(astraObj.castStringToInt("abc"))
+// console.log(astraObj.castStringToInt("bca"))
+// console.log(astraObj.castStringToInt("cba"))
+// console.log(astraObj.castStringToInt("cab"))
+// console.log(astraObj.castStringToInt("bac"))
+// console.log(astraObj.castStringToInt("g"))
